046380e01a9b6c891669a254385ec0cd71ed5bb9
kevin_ducrot <ducrot.kevin5@gmail.com>
2026-01-16 13:32:10 +0100

    maj info log test

--- partie_3_chat.py
+++ partie_3_chat.py
@@ -113,5 +113,5 @@ elif st.session_state["authentication_status"] is False:
     st.error("L'username ou le password est/sont incorrect")
 elif st.session_state["authentication_status"] is None:
     st.warning('Les champs username et mot de passe doivent être remplie')
-    st.write("username = utilisateur")
-    st.write("password = utilisateurMDP")
+    st.info("username = utilisateur")
+    st.info("password = utilisateurMDP")
